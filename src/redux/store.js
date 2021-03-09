0969819e35488d94c893cd95ed1ba62e15f863a2
Rockindude <deschmidt83@gmail.com>
2021-03-09 15:19:23 +0100

    removing logger in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import {persistStore} from "redux-persist";
 import rootReducer from "./rootReducer";
 
 // Middleware nimmt die action und console.loged sie, dann geht es weiter
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV == "development"){
+  middlewares.push(logger);
+}
 
 // Durch den Spread werden die Array-elemente eigene Parameter
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
